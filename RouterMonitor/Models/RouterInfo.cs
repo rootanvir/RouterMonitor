631a712afb8a1e9004769287f3ce34fd814a0d99
rootanvir <tanvirahmed131015@gmail.com>
2026-06-26 01:14:29 +0600

    routerinfo.variable

--- RouterMonitor/Models/RouterInfo.cs
+++ RouterMonitor/Models/RouterInfo.cs
@@ -2,12 +2,18 @@
 {
     public class RouterInfo
     {
+        public string? SSID { get; set; }
         public string? GatewayIP { get; set; }
-        public string? AdapterName { get; set; }
-        public string? AdapterDescription { get; set; }
         public string? LocalIP { get; set; }
         public string? SubnetMask { get; set; }
         public string? DnsServers { get; set; }
+        public string? AdapterName { get; set; }
+        public string? AdapterDescription { get; set; }
         public string? MacAddress { get; set; }
+        public string? SignalStrength { get; set; }
+        public string? Channel { get; set; }
+        public string? RecieveRate { get; set; }
+        public string? TransmitRate { get; set; }
+
     }
 }
